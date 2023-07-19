59fd0cfff3b746af3e9adcc49a1ade0c26a06c5b
evgenyrinkevich <rinkevich2000@gmail.com>
2023-07-19 17:09:10 +0300

    10- Exercise- Creating a Provider (2:51)

--- src/state-management/TaskList.tsx
+++ src/state-management/TaskList.tsx
@@ -1,8 +1,7 @@
-import { useContext } from 'react';
-import TasksContext from './contexts/tasksContext';
+import useTasks from './hooks/useTasks';
 
 const TaskList = () => {
-  const { tasks, dispatch } = useContext(TasksContext);
+  const { tasks, dispatch } = useTasks();
 
   return (
     <>
