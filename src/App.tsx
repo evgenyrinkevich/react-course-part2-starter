b78106b3b947c7931f8b46199511ee076764e060
evgenyrinkevich <rinkevich2000@gmail.com>
2023-07-19 16:57:02 +0300

    8- Creating a Custom Provider (2:58)

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,18 @@ import tasksReducer from './state-management/reducers/tasksReducer';
 import HomePage from './routing/HomePage';
 import NavBar from './state-management/NavBar';
 import TasksContext from './state-management/contexts/tasksContext';
-import authReducer from './state-management/reducers/authReducer';
-import AuthContext from './state-management/contexts/authContext';
+import AuthProvider from './state-management/AuthProvider';
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, '');
 
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
         <NavBar />
         <HomePage />
       </TasksContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
